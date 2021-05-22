ea51e9f8d38f6baa9d0fea278e09435da9de0f8e
Jesse Orozco <orozcojesse0@gmail.com>
2021-05-22 13:29:47 -0400

    added index.js file to seed

--- models/index.js
+++ models/index.js
@@ -2,25 +2,25 @@ const Post = require("./post");
 const Comment = require("./comment");
 const User = require("./user");
 
-// Post.hasMany(Comment, {
-//   foreignKey: "postId",
-// });
+Post.hasMany(Comment, {
+  foreignKey: "postId",
+});
 
-// Post.belongsTo(User, {
-//   foreignKey: "userId",
-// });
+Post.belongsTo(User, {
+  foreignKey: "userId",
+});
 
-// Comment.belongsTo(User, {
-//   foreignKey: "userId",
-// });
+Comment.belongsTo(User, {
+  foreignKey: "userId",
+});
 
-// User.hasMany(Post, {
-//   foreignKey: "postId",
-// });
+User.hasMany(Post, {
+  foreignKey: "postId",
+});
 
-// User.hasMany(Comment, {
-//   foreignKey: "userId",
-// });
+User.hasMany(Comment, {
+  foreignKey: "userId",
+});
 
 module.exports = {
   User,
