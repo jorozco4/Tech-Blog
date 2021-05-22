597fcb04318b566c26c66c2a4fee3e1fc9e755e5
Jesse Orozco <orozcojesse0@gmail.com>
2021-05-22 10:14:26 -0400

    fixed issued resolving sequlize error in user.js

--- models/index.js
+++ models/index.js
@@ -1,18 +1,26 @@
-const User = require("./user");
 const Post = require("./post");
 const Comment = require("./comment");
+const User = require("./user");
 
-Post.hasMany(Comment, {
-  foreignKey: "postId",
-});
+// Post.hasMany(Comment, {
+//   foreignKey: "postId",
+// });
 
-Post.belongsTo(User, {
-  foreignKey: "userId",
-});
+// Post.belongsTo(User, {
+//   foreignKey: "userId",
+// });
 
-Comment.belongsTo(User, {
-  foreignKey: "userId",
-});
+// Comment.belongsTo(User, {
+//   foreignKey: "userId",
+// });
+
+// User.hasMany(Post, {
+//   foreignKey: "postId",
+// });
+
+// User.hasMany(Comment, {
+//   foreignKey: "userId",
+// });
 
 module.exports = {
   User,
--- models/user.js
+++ models/user.js
@@ -8,26 +8,27 @@ class User extends Model {
   }
 }
 
-User.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
 
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [8],
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
     },
   },
-
   // hooks: {
   //   beforeCreate: async (newUserData) => {
   //     newUserData.password = await bcrypt.hash(newUserData.password, 10);
@@ -41,11 +42,13 @@ User.init({
   //     return updatedUserData;
   //   },
   // },
-  sequelize,
-  timestamps: false,
-  freezeTableName: true,
-  underscored: true,
-  modelName: "User",
-});
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Users",
+  }
+);
 
 module.exports = User;
